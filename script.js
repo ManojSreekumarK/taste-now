649820d122bdde8ec689e2e07cd3c542c7d400df
ManojSreekumarK <manojsreekumar2000@gmail.com>
2023-04-13 01:55:46 +0530

    anima

--- script.js
+++ script.js
@@ -27,6 +27,45 @@ window.addEventListener("resize", function () {
     menu.classList.remove("active");
   }
 });
+// Get the element to be faded
+const element = document.querySelector(".fade-in-out-left");
+
+// Check if the element is visible
+function isVisible(element) {
+  const elementPosition = element.getBoundingClientRect();
+  return (
+    elementPosition.top < window.innerHeight && elementPosition.bottom >= 0
+  );
+}
+
+// Add an event listener to the scroll event
+window.addEventListener("scroll", () => {
+  if (isVisible(element)) {
+    element.classList.add("visible-left");
+  } else {
+    element.classList.remove("visible-left");
+  }
+});
+
+const elements = document.querySelector(".fade-in-out-right");
+
+// Check if the element is visible
+function isVisible(elements) {
+  const elementPosition = elements.getBoundingClientRect();
+  return (
+    elementPosition.top < window.innerHeight && elementPosition.bottom >= 0
+  );
+}
+
+// Add an event listener to the scroll event
+window.addEventListener("scroll", () => {
+  if (isVisible(elements)) {
+    elements.classList.add("visible-right");
+  } else {
+    elements.classList.remove("visible-right");
+  }
+});
+
 let categories = [
   {
     item: "pizza",
